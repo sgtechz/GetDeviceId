a1218fcc78447ab803631befec8c80a41fcb9450
Samuel Gonzale <sgonzalez@encodesa.com.ar>
2021-03-30 11:54:44 -0300

    first commit

---
+++ deviceId-main/AppPrueba/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPrueba
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppPrueba.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ deviceId-main/AppPrueba/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Plugin.DeviceInfo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPrueba
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var deviceId = CrossDeviceInfo.Current.Id;
+            var deviceIdResult = new Label();
+
+            deviceIdResult.Text = deviceId;
+
+            titleDeviceId.Text = "My device Id: " + deviceIdResult.Text;
+        }
+    }
+}
